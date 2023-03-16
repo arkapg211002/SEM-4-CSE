75711ea61a63964292201880f95df2e02a0b4653
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-03-16 21:38:08 +0530

    Add files via upload

---
+++ Design and Analysis of Algorithms/Assignment12.1/q.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int count(int *a, int x)
+{
+        int i,c=0;
+        for(i=0;i<10;i++)
+        {
+                if(a[i]==x)
+                        c++;
+        }
+        return c;
+}
+int valid(int *a)
+{
+    int i,f=0;
+    for(i=0;i<10;i++)
+    {
+        if(a[i]!=count(a,i))
+                {
+                        f=1;
+                        break;
+                }
+    }   
+}
+void transform(int *a)
+{
+        int i=0;
+        while(valid(a)!=0)
+        {
+                for(i=0;i<10;i++)
+                {
+                      a[i]=count(a,i);
+                }
+        }      
+}
+void main()
+{
+        int a[]={9,0,0,0,0,0,0,0,0,1};
+        int i,j;
+        for(i=0;i<10;i++)
+        {
+                if(a[i]!=count(a,i))
+                        transform(a);
+        }
+        printf("\nThe required 10-digit number: \n");
+        for(i=0;i<10;i++)
+                printf("%d",a[i]);   
+        printf("\n");                 
+}
