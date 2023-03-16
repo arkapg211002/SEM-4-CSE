71e6cd244b339ae7ad2910748c90b18f312340b9
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-03-16 21:38:56 +0530

    Update q.c

--- Design and Analysis of Algorithms/Assignment12.1/q.c
+++ Design and Analysis of Algorithms/Assignment12.1/q.c
@@ -1,3 +1,10 @@
+/*
+Find the only 10 digit number where the digit at the ith position (i = 0, 1, 2, 3 etc.) 
+from left specifies the number of occurrences of digit ' i ' in the given number.
+*/
+
+
+
 #include <stdio.h>
 int count(int *a, int x)
 {
